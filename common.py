8cb4b14cc24a7bff5ebbd8c1a66586c5238cf0ea
Nosgoroth <nosgoroth@gmail.com>
2020-08-30 11:24:25 +0200

    Optionally log send to kindle output

--- common.py
+++ common.py
@@ -14,10 +14,19 @@ def process_new_file(file, storeName):
     os.chdir(dirname)
 
     try:
-        if config["USE_SEND_TO_KINDLE"]:
+        if "USE_SEND_TO_KINDLE" in config and config["USE_SEND_TO_KINDLE"]:
             fsize = os.stat(fn).st_size
             if fsize < config["KINDLE_MAX_FSIZE"]:
-                subprocess.call(config["SEND_TO_KINDLE_COMMAND"] + [fn])
+                print("Sending to kindle: %s" % basefn)
+                
+                cmd = config["SEND_TO_KINDLE_COMMAND"] + [fn]
+                
+                if "SEND_TO_KINDLE_LOGFILE" in config and config["SEND_TO_KINDLE_LOGFILE"]:
+                    with open(config["SEND_TO_KINDLE_LOGFILE"], 'a') as logfile:
+                        subprocess.call(cmd, stdout=logfile, stderr=logfile)
+                else:
+                    subprocess.call(cmd)
+                
                 pushover("[ESSYNC:%s] Sent to Kindle new epub: %s" % (storeName, basefn))
             else:
                 pushover("[ESSYNC:%s] Downloaded large epub: %s" % (storeName, basefn))
